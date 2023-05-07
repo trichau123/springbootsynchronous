a60fa8d540ce4eb06fc808c8ad5f4ce78d2b1d40
trichau123 <53028788+trichau123@users.noreply.github.com>
2023-05-07 11:28:49 -0400

    hello

---
+++ src/main/java/com/demo/demosynchronousproject/DemoSynchronousProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demosynchronousproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSynchronousProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSynchronousProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/demosynchronousproject/DemoSynchronousProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demosynchronousproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSynchronousProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
